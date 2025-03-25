5a8496acb4c6c0c1cc8693389cfa08b4ade8e4e4
colleague <GuzenkoEA@yandex.ru>
2025-03-25 10:21:12 +0300

    'next4_add_box'

--- app/src/main/java/com/example/the_planner_semen/MainActivity.kt
+++ app/src/main/java/com/example/the_planner_semen/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +18,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -29,13 +34,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Surface {
-                OrderList(generateOrders())
+                CustomContainer {
+                    OrderList(generateOrders())
+                }
             }
         }
     }
 }
 
-
+@Composable
+fun CustomContainer(content: @Composable () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxSize().padding(top = 38.dp, bottom = 47.dp)
+    ) {
+        content()
+    }
+}
 
 data class Order(val title: String, val creationDate: String)
 fun generateOrders(): List<Order> {
